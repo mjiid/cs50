0f7920edd9020917190dc1bcd559a90a742f92a0
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-24 22:53:17 +0000

    Sun, Jul 24, 2022, 10:53 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,13 +5,13 @@
 int main(void)
 {
     long cardNum=0;
-    int sumOfProducts=0;
     cardNum=get_long("Enter your credit card number: \n");
     //Check if the number contains 13, 15 or 16 digits
     int numDig=floor(log10(labs( cardNum)) )+1;
     if(numDig<13 || numDig>16 || numDig==14){
         printf("INVALID\n");
     }
+
     //calculate the sum of the products:
     int sum1=0,sum2=0,total=0;
     for(int i=0;i<numDig;i++){
@@ -34,10 +34,14 @@ int main(void)
             sum2+=remain;
         }
     }
+    //First digit of the card number:
+    while(cardNum){
+        
+    }
     total=sum1+sum2-1;
     printf("%i\n",total);
     if(!(total%10)){
-        
+
     }else{
         printf("INVALID\n");
     }
