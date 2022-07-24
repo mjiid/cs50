89da42474506324e96204a19d62a45ecf5ce62ce
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-24 16:18:43 +0000

    Sun, Jul 24, 2022, 4:18 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -24,11 +24,7 @@ int main(void)
             printf("%i\n",num);
         }
         while(num>0){
-            num=num*2;
-            int rem=num%10;
-            num=num/10;
-            sum+=rem;
-            printf("%i\n",rem);
+            
         }
 
     }
