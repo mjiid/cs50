24496d58b8c4982d04045e172b24ff84deee8920
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-24 10:30:07 +0000

    Sun, Jul 24, 2022, 10:30 AM +00:00

---
+++ mario-more/mario.c
@@ -0,0 +1,36 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 0;
+    while(height >8 || height <1){
+        height = get_int("Enter the height that you want: \n");
+    }
+    for(int i=0; i < height; i++)
+    {
+        for(int j=0; j<height*2 + 2; j++)
+        {
+           if(j<height){
+                if(height-j-1 > i){
+                    printf(" ");
+                }else
+                {
+                    printf("#");
+                }
+           }else
+           {
+            if(j==height+1 || j==height +2){
+                printf(" ");
+            }else{
+                if(j<=2*height + i+1 && j > height){
+                    printf("#");
+                }
+            }
+           }
+        }
+        printf("\n");
+    }
+
+}
