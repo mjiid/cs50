396250dc44771c20a933e1babeaff865825402ef
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-24 11:55:55 +0000

    Sun, Jul 24, 2022, 11:55 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,7 +6,7 @@ int main(void)
 {
     long cardNum=0;
     cardNum=get_long("Enter your credit card number: \n");
-    int numDig=floor(log10(abs(cardNum)) +1);
+    int numDig=floor(log10(abs(cardNum)) )+1;
     if(numDIg<13 || numDig>16 || numDIg==14){
         printf("INVALID");
     }
