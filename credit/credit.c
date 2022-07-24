fe73023fade3e254ae753d25601a128ab26ae7ff
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-24 12:04:08 +0000

    Sun, Jul 24, 2022, 12:04 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,13 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-
 int main(void)
 {
     long cardNum=0;
     cardNum=get_long("Enter your credit card number: \n");
-    int numDig=floor(log10(abs(cardNum)) )+1;
-    if(numDIg<13 || numDig>16 || numDIg==14){
+    int numDig=floor(log10(labs( cardNum)) )+1;
+    if(numDIg<13 || numDig>16 || numDig==14){
         printf("INVALID");
     }
 
