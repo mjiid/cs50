88292b61cf45d0b48c2fe1d7dcc554fc5db5acca
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-27 10:51:56 +0000

    Wed, Jul 27, 2022, 10:51 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -38,7 +38,7 @@ int compute_score(string word)
         character=toupper(character);
         if((int) character >65 && (int) character <90){
             printf("%i and the score is %i\n",(int) character - 64,POINTS[(int) character - 64]);
-            score+=POINTS[(int) character - 64];
+            score+=POINTS[(int) character - 65];
         }
     }
     return score;
