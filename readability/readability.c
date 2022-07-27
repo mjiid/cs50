0b09384352ecd20b61593ca405b7eb121178ad4f
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-27 11:20:34 +0000

    Wed, Jul 27, 2022, 11:20 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,5 +4,19 @@
 int main(void)
 {
     string text=get_string("Text: ");
-    
+    int words=0,letters=0,sentences=0,Grad;
+    for( int i=0,n=strlen(text);i<n;i++)
+    {
+        if(text[i]=='.' || text[i]=='?'|| text[i]=='!')
+        {
+            sentences++;
+        }else if(text[i]==' ')
+        {
+            words++;
+        }else
+        {
+            letters++;
+        }
+    }
+    return
 }
