b7460e5b2de91966fc8c95f0f5fb1492ee50bdd2
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-28 20:39:13 +0000

    Thu, Jul 28, 2022, 8:39 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,44 +7,27 @@
 bool only_digits(string anykey);
 int main(int argc, string argv[])
 {
-    int key= atoi(argv[1]);
-    if(argc<2)
+    int k = atoi(argv[1]);
+    if(argc!=2)
     {
-        printf("Error \n");
+        printf("Error!\n");
         return 1;
     }
-    else if( argc>2)
-    {
-        printf("Usage: ./caesar key \n");
-        return 1;
-    }
-    else
-    {
-        string plaintext=get_string("plaintext : ");
-        string ciphertext;
-        for (int i = 0, n = strlen(plaintext);i<n;i++)
-        {
-            if(((int) plaintext[i] >= 65 && (int) plaintext[i] <= 90) || ((int) plaintext[i] >= 97 && (int) plaintext[i] <= 122))
-            {
-                while (key >=26)
-                {
-                    key = key - 26;
-                }
 
-            }
-        }
-        printf("the key is : %i",key);
-    }
+
 }
+
+
 
 bool only_digits(string anykey)
 {
-    for (int i =0 , n = strlen(anykey);i<n;i++)
+    for (int i = 0, n= strlen(anykey);i<n;i++)
     {
-        if( atoi(anykey[i]) +48 > 48 || atoi(anykey[i]) +48 < 57)
+        int keynum = atoi(anykey);
+        if(keynum +48<48 || keynum+48<57)
         {
-            return false;
+            printf("Usage: ./caesar key\n");
+            return 1;
         }
     }
-    return true;
 }
