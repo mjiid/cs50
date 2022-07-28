2dc24b7057c52ed5e5010b9a50f3f498af20ddf7
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-28 14:31:59 +0000

    Thu, Jul 28, 2022, 2:31 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,13 +4,5 @@
 
 int main(int argc, string argv[])
 {
-    int key= atoi(argv[1]);
-    int *keyAdd= &key;
-    printf("%d\n",&keyAdd);
-    if(keyAdd==NULL){
-        printf("Error\n");
-    }else{
-        string plaintext=get_string("Plaintext: ");
-    }
-
+    printf("%s\n",argv[1]);
 }
