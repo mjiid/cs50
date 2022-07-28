62b55802efb73e4658d58c6844c9f6310c0d416f
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-28 14:37:27 +0000

    Thu, Jul 28, 2022, 2:37 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,14 +4,5 @@
 
 int main(int argc, string argv[])
 {
-    if(argv[1]=='(null)')
-    {
-        printf("Error\n");
-    }
-    else
-    {
-        int key = atoi(argv[1]);
-
-    }
-
+    printf("%s\n",argv[1]);
 }
