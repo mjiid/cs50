6c5b034008e7ca7292c20e9627b9d090044b49aa
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-29 16:16:02 +0000

    Fri, Jul 29, 2022, 4:16 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,12 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     string key=argv[1];
-    if(argc!=2)
+    if(argc!=2) // the user doesn't provide a key at all or more than one key.
     {
         printf("Usage: ./substitution key\n");
         return -1;
+    }
+    else if(strlen(key)!=26)
+    {
+        printf("Key must contain 26 characters.\n");
     }
 }
