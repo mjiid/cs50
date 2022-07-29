cc48653ba021b592a41accf4e05577df4e2f0a9c
mjiid <100082612+mjiid@users.noreply.github.com>
2022-07-29 16:12:18 +0000

    Fri, Jul 29, 2022, 4:12 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    string key=argv[1];
+    if(argc!=2)
+    {
+        printf("")
+        return -1;
+    }
 }
