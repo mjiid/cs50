ae1b01761b2913cb5c3e95aa88e3a8e435bcc28c
mjiid <100082612+mjiid@users.noreply.github.com>
2022-08-16 18:46:18 +0000

    ccc, August 16, 2022 6:46 PM 2, 1818, 6:46 pm +0000

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,7 +69,7 @@ bool vote(string name)
     // TODO
     for(int i = 0; i < candidate_count; i++)
     {
-        if(strcmp(name,candidate[i]))
+        if(strcmp(name, candidate[i].name))
         {
             candidate[i].votes++;
             return true;
