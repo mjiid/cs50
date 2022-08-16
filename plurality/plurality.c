77cbac67031edfdb26b3044f4bab30acc1017c1e
mjiid <100082612+mjiid@users.noreply.github.com>
2022-08-16 17:01:08 +0000

    ccc, August 16, 2022 5:01 PM 2, 1717, 5:01 pm +0000

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,7 +67,10 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp())
+    }
     return false;
 }
 
