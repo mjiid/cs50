11a35631f1ab4469f04885c5955a60e1a0abe76a
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-05 19:52:48 +0000

    Sat, Nov 5, 2022, 7:52 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -12,9 +12,9 @@ int main(void)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 2*n+2; j++)
+        for (int j = 0; j < n; j++)
         {
-            if(j >= n-i-1 && j < )
+            if(j >= n-i-1)
             {
                 printf("#");
             }
