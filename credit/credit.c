56cef349e15506908a0a9e78092425904d6d8fb3
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-06 16:13:34 +0000

    Sun, Nov 6, 2022, 4:13 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,42 +2,80 @@
 #include <stdio.h>
 
 int digits(long num);
+int first_dig(long num);
+int two_digs(long num);
 
 int main(void)
 {
     long card_num = get_long("Enter your credit card number :");
     int digit = digits(card_num);
+
+    //validating the number of digits :
     if (digit != 13 && digit != 15 && digit != 16)
     {
         printf("INVALID\n");
-        return -1;
+        return 0;
     }
     else
     {
+        int first = first_dig(card_num);
+        int two = two_digs(card_num);
         long card_cpy = card_num;
-        printf("%li\n",card_cpy);
-        int remainder = 0;
-        int  k = 0, sum1 = 0, sum2 = 0;;
+        int remainder = 0,rem = 0;
+        int  k = 0, sum1 = 0, sum2 = 0, total = 0;
+
+        //validating the card by Luhn's algorithm:
         while (card_cpy > 0)
         {
+            //getting the digits of the number:
             remainder = card_cpy % 10;
             card_cpy = card_cpy / 10;
             if (k)
             {
-                remainder = 2 * remainder;
+                remainder = remainder * 2;
+                //getting the digits of the remainder:
                 while (remainder > 0)
                 {
-                    remainder = remainder % 10;
-                    sum1 += remainder;
+                    rem = remainder % 10;
+                    remainder = remainder / 10;
+                    sum1 += rem;
                 }
+                k = 0;
             }
             else
             {
-                
+                sum2 += remainder;
+                k=1;
             }
         }
-        printf("%d\n",sum1);
-        printf("%d\n",sum2);
+        total = sum1 + sum2;
+        // validate the card :
+        if ( (total % 10) != 0 )
+        {
+            printf("INVALID\n");
+            return 0;
+        }
+        else if (first == 4)
+        {
+            printf("VISA\n");
+            return 0;
+        }
+        else if (two == 34 || two == 37)
+        {
+            printf("AMEX\n");
+            return 0;
+        }
+        else if (two == 51 ||two == 52 ||two == 53 ||two == 54)
+        {
+            printf("MASTERCARD\n");
+            return 0;
+        }
+        else
+        {
+            printf("INVALID\n");
+            return 0;
+        }
+
 
     }
 
@@ -57,4 +95,25 @@ int digits(long num)
         dig++;
     }
     return dig;
+}
+
+//determines the first digit of a given number :
+int first_dig(long num)
+{
+    while (num > 10)
+    {
+        num = num / 10;
+    }
+    return num;
+}
+
+//determines the second digit of a given number :
+
+int two_digs(long num)
+{
+    while (num > 100)
+    {
+        num = num / 10;
+    }
+    return num;
 }
