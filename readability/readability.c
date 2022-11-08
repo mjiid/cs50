41416f38ba1ccfe6f94ff01bfbacb6c6765cb514
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-08 10:43:23 +0000

    Tue, Nov 8, 2022, 10:43 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -19,7 +19,15 @@ int main(void)
 
     if (index < 1)
     {
-        printf("");
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %d\n",index);
     }
 }
 
