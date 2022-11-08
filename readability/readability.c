cca762e2960147212ecb83df22b54f365f39dde1
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-08 09:40:55 +0000

    Tue, Nov 8, 2022, 9:40 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,9 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int count_letters(string text);
 
 int main(void)
 {
     string text = get_string("Text : ");
-    printf("%s\n",text);
-    
+    int letters = count_letters(text);
+    printf("%d\n",letters);
+
+}
+
+
+
+int count_letters(string text)
+{
+    int num = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (islower(text[i]) || isupper(text[i]))
+        {
+            num++;
+        }
+    }
+    return num;
 }
