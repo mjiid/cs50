936fa832e3f4ecea2130bfeb19c921d277c313bb
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-08 09:57:01 +0000

    Tue, Nov 8, 2022, 9:57 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -44,7 +44,7 @@ int count_words(string text)
             num++;
             space = 1;
         }
-        else if ()
+        else if (text[i] == ' ' && space == 1)
         {
             space = 0;
         }
