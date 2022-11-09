7fa2f5502326aef6b2c0971363553365f4558ba7
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-09 17:46:54 +0000

    Wed, Nov 9, 2022, 5:46 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,7 +143,14 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated)
+        {
+            candidates[i].votes++;
+        }
+
+    }
     return;
 }
 
