2a115a152c959f2ab3f1933225fb6e4054615763
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-10 20:37:23 +0000

    Thu, Nov 10, 2022, 8:37 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -164,9 +164,9 @@ bool print_winner(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > voter_count/2)
+        if (candidates[i].votes > voter_count / 2)
         {
-            printf("%s\n",candidates[i].name);
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }
