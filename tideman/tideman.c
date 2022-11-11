4dc853ec686749f9e532e1d6951f06c45912857e
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-11 08:52:22 +0000

    Fri, Nov 11, 2022, 8:52 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -134,13 +134,25 @@ void add_pairs(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (i =! j)
+            if (i == j)
             {
                 continue;
             }
             else if (preferences[i][j] > preferences[j][i])
             {
-                pairs.
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
+            else
+            {
+                continue;
             }
         }
     }
