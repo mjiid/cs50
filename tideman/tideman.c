332461733e268f04ac4ae6de61a75eb607ca70ad
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-11 08:58:24 +0000

    Fri, Nov 11, 2022, 8:58 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,7 +132,7 @@ void add_pairs(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 0; j != i && j < candidate_count; j++)
         {
             if (preferences[i][j] > preferences[j][i])
             {
