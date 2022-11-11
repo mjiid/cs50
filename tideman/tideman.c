2e8f968f1abf77f1827b84c5957718d4a9978d3d
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-11 15:46:59 +0000

    Fri, Nov 11, 2022, 3:46 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -186,15 +186,6 @@ void lock_pairs(void)
     // TODO
     for (int i = 0; i < pair_count; i++)
     {
-        int count = 0;
-        for (int j = i + 1; j < pair_count; j++)
-        {
-            if (pairs[i].winner == pairs[j].winner)
-            {
-                count++;
-            }
-        }
-        
         locked[pairs[i].winner][pairs[i].loser] = true;
     }
     return;
