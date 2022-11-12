b80043094e2c78ec3b7dea347e9df98878179e5e
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-12 14:29:30 +0000

    Sat, Nov 12, 2022, 2:29 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -182,7 +182,21 @@ void sort_pairs(void)
 
 
 // a function that detects if there is a cycle or not:
-bool 
+bool has_cycle(int winner, int loser)
+{
+    if (locked[loser][winner] == true)
+    {
+        return true;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (locked[loser][i] == true && has_cycle(winner,i))
+        {
+            return true;
+        }
+    }
+    return true;
+}
 
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
