6aad87d7ea475130ed8a83b735c0fb74068ca7b6
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-14 21:46:15 +0000

    Mon, Nov 14, 2022, 9:46 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,11 +7,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE pixel = image[i][j];
-            int average =  (pixel.rgbtBlue + pixel.rgbtRed + pixel.rgbtGreen) / 3;
-            pixel.rgbtBlue = average;
-            pixel.rgbtRed = average;
-            pixel.rgbtGreen = average;
+            int average =  (image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3;
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
         }
     }
     return;
