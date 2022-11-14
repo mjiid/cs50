23de6f3573c3c0ee95ca564871bc28116bf54124
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-14 21:29:43 +0000

    Mon, Nov 14, 2022, 9:29 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,7 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            int average = 
+        }
+    }
     return;
 }
 
