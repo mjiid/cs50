14b4d290b908f2721844071c67bae8b8270d6752
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-14 22:10:38 +0000

    Mon, Nov 14, 2022, 10:10 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -20,6 +20,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            image[i][j] = image[i][width - j];
+            image[i][width - j] = pixel;
+        }
+    }
     return;
 }
 
