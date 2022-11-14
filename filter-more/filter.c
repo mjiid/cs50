6d33fabfa33812633a7c91962977438d37fc7dc9
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-14 22:01:16 +0000

    Mon, Nov 14, 2022, 10:01 PM +00:00

--- filter-more/filter.c
+++ filter-more/filter.c
@@ -1,7 +1,6 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "helpers.h"
 
 int main(int argc, char *argv[])
--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int average =  (image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3;
+            int average =  roundf((image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3);
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtRed = average;
             image[i][j].rgbtGreen = average;
