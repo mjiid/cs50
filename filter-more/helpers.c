579eff91d05d88c8bed84b9241a2697a5e4c837e
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-15 16:24:23 +0000

    Tue, Nov 15, 2022, 4:24 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -46,26 +46,31 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            float count = 0;
             for (int k = i - 1; k <= i + 1; k++)
             {
-                for (int l = j - 1; l <= j + 1; j++)
+                for (int l = j - 1; l <= j + 1; l++)
                 {
                     if (k < 0 || k > height || l < 0 || l > width)
                     {
                         continue;
                     }
-                    else if (k == i && l == j)
+                    else if (k == i && l ==j)
                     {
                         continue;
                     }
                     else
                     {
-                        image[i][j].rgbtBlue += image[k][l].rgbtBlue;
-                        image[i][j].rgbtRed += image[k][l].rgbtRed;
-                        image[i][j].rgbtGreen += image[k][l].rgbtGreen;
+                        image[i][j].rgbtBlue = image[i][j].rgbtBlue + image[k][l].rgbtBlue;
+                        image[i][j].rgbtRed = image[i][j].rgbtRed + image[k][l].rgbtRed;
+                        image[i][j].rgbtGreen = image[i][j].rgbtGreen + image[k][l].rgbtGreen;
+                        count++;
                     }
                 }
             }
+            image[i][j].rgbtBlue = roundf(image[i][j].rgbtBlue / count);
+            image[i][j].rgbtRed = roundf(image[i][j].rgbtRed / count);
+            image[i][j].rgbtGreen = roundf(image[i][j].rgbtGreen / count);
         }
     }
     return;
