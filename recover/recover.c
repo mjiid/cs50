6c13c42db1cc31a6e9f948a5a2e182db96a184b6
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-20 14:34:00 +0000

    Sun, Nov 20, 2022, 2:34 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
             {
                 sprintf(filename, "%03i.jpg", count);
                 img = fopen(filename, "w");
-                fwrite(buffer, 1, BLOCK_SIZE, img);
+                fwrite(buffer, BLOCK_SIZE, 1, img);
                 found = 1;
             }
             else
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
                 fclose(img);
                 sprintf(filename, "%03i.jpg", count);
                 img = fopen(filename, "w");
-                fwrite(buffer, 1, BLOCK_SIZE, img);
+                fwrite(buffer, BLOCK_SIZE, 1, img);
                 found = 0;
             }
             count++;
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
         {
             if (found)
             {
-                fwrite(buffer, 1, BLOCK_SIZE, img);
+                fwrite(buffer, BLOCK_SIZE, 1, img);
             }
         }
 
