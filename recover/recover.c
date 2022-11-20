e2f20d3525f8a0386b413daf52211db716fb9b4d
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-20 11:10:41 +0000

    Sun, Nov 20, 2022, 11:10 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,11 +22,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    BYTE buffer;
+    
 
-    while (fopen(buffer, 1, 512, forensic_img) == 512)
+    while (fread(buffer, 1, 512, forensic_img) == 512)
     {
-        
+
     }
 
 }
