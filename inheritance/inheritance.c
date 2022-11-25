1e6b73336588594f839f72dcbf08cc79130385d8
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-25 10:08:17 +0000

    Fri, Nov 25, 2022, 10:08 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *person_loc = malloc(sizeof(person));
+    person *new_person = malloc(sizeof(person));
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -49,9 +49,10 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        
+        new_person.parents[0] = parent0;
+        new_person.parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-
+        
     }
 
     // If there are no generations left to create
