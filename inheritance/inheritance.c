7bb84545607d6a396df96a82c7a45759d8153c5f
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-25 10:24:02 +0000

    Fri, Nov 25, 2022, 10:24 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,8 +52,8 @@ person *create_family(int generations)
         new_person->parents[0] = parent0;
         new_person->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] = parent0->alleles[rand() % 3];
-        new_person->alleles[1] = parent0->alleles[rand() % 3];
+        new_person->alleles[0] = parent0->alleles[rand() % 2];
+        new_person->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
