17f0bf31ca6d0d8dab13d3a25ad3622fc3e80b4b
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-25 10:03:57 +0000

    Fri, Nov 25, 2022, 10:03 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-
+    person_loc = malloc(sizeof(person));
     // If there are still generations left to create
     if (generations > 1)
     {
