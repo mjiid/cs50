76a36ddca473316f5035b2c952596f637cb252da
mjiid <100082612+mjiid@users.noreply.github.com>
2022-11-30 14:54:30 +0000

    Wed, Nov 30, 2022, 2:54 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -17,7 +17,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 676;
 
 // Hash table
 node *table[N];
@@ -33,7 +33,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    
 }
 
 // Loads dictionary into memory, returning true if successful, else false
