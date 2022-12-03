c822f3bb1f29831052ac8c08b6ab1c533bb36497
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-03 14:57:25 +0000

    Sat, Dec 3, 2022, 2:57 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -12,4 +12,56 @@ def main():
         return 0
     else:
         first = int(num_list[0])
-        
+        sec = int(num_list[1])
+        card_cpy = card_num
+        remainder, rem = 0, 0
+        k, sum1, sum2, total = 0, 0, 0, 0
+
+        while card_cpy > 0:
+            remainder = card_cpy % 10
+            card_cpy /= 10
+            if k:
+                remainder *= 2
+                while remainder > 0:
+                    rem = remainder % 10
+                    remainder /= 10
+                    sum1 += rem
+                k = 0
+            else:
+                sum2 += remainder
+                k = 1
+
+            else
+            {
+                sum2 += remainder;
+                k = 1;
+            }
+        }
+        total = sum1 + sum2;
+        // validate the card :
+        if ((total % 10) != 0)
+        {
+            printf("INVALID\n");
+            return 0;
+        }
+        else if (first == 4)
+        {
+            printf("VISA\n");
+            return 0;
+        }
+        else if (two == 34 || two == 37)
+        {
+            printf("AMEX\n");
+            return 0;
+        }
+        else if (two == 51 || two == 52 || two == 53 || two == 54 || two == 55)
+        {
+            printf("MASTERCARD\n");
+            return 0;
+        }
+        else
+        {
+            printf("INVALID\n");
+            return 0;
+        }
+
