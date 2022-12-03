1c09b9bc8acf09b6b0ff4d8b6981a1fb50895dd3
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-03 11:13:43 +0000

    Sat, Dec 3, 2022, 11:13 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -23,7 +23,7 @@ def main():
             teams.append(row)
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-
+    
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
@@ -54,7 +54,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    
+    while len(teams) =! 1:
+        teams = simulate_round(teams)
+    return teams
 
 
 if __name__ == "__main__":
