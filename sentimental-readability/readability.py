97ccf61c9c52e3011e0f7652b4afce2d321f5650
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-03 15:29:54 +0000

    Sat, Dec 3, 2022, 3:29 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,3 +1,16 @@
 # TODO
 from cs50 import get_string
 
+text = get_string("Text: ")
+
+letters = count_letters(text)
+words = count_words(text)
+sentences = count_sentences(text)
+
+
+
+
+def count_letters(text):
+    count = 0
+    for ch in text:
+        if 
