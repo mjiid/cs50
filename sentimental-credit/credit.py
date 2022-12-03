1fe818c58aefb9acc9a485f27acb2649f9a2b6a9
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-03 14:51:23 +0000

    Sat, Dec 3, 2022, 2:51 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,4 +2,9 @@
 from cs50 import get_int, get_string
 import re
 
-number = get_int("Number: ")
+def main():
+    number = get_int("Number: ")
+    digits = len(re.findall('[0-9]', str(number)))
+    if digits != 13 and digits != 15 and digits != 16:
+        print(INVALID)
+        return 0
