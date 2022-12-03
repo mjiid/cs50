30d5191b9ddc600c5937c9b0a1b695aef8c0abf0
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-03 14:22:51 +0000

    Sat, Dec 3, 2022, 2:22 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,8 +1,19 @@
 # TODO
-import cs50
+from cs50 import get_int
 
 while True:
     n = get_int("Enter the number of rows: ")
     if n >= 1 and n <= 8:
         break
 
+for i in range(n):
+    num = 0
+    for j in range(2*n + 2):
+        if (j < n and j >= n - i - 1) or (j >= n + 2 and j <= n + 2 +i):
+            print("#", end="")
+            num += 1
+        elif num == 2*(i+1):
+            break
+        else:
+            print(" ", end="")
+    print()
