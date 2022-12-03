b0e2709a55ed0366fad1f3482f0daeb0bbdbaffc
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-03 15:07:50 +0000

    Sat, Dec 3, 2022, 3:07 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,18 +6,16 @@ def main():
     number = get_int("Number: ")
     num_list = re.findall('[0-9]', str(number))
     digits = len(num_list)
-    print(digits)
     if (digits != 13 and digits != 15 and digits != 16):
+        print("5")
         print(INVALID)
         return 0
     else:
         first = int(num_list[0])
         sec = int(num_list[0] + num_list[1])
-        print(first)
-        print(sec)
         card_cpy = number
         remainder, rem = 0, 0
-        k, sum1, sum2, total = 0, 0, 0, 0
+        k, sum1, sum2 = 0, 0, 0
 
         while card_cpy > 0:
             remainder = card_cpy % 10
@@ -35,18 +33,23 @@ def main():
         total = sum1 + sum2
         if (total % 10 != 0):
             print("INVALID")
+            print("1")
             return 0
         elif first == 4:
             print("VISA")
+            print("2")
             return 0
         elif (sec== 34 or sec == 37):
             print("AMEX")
+            print("3")
             return 0
         elif (sec == 51 or sec == 52 or sec == 53 or sec == 54 or sec== 55):
             print("MASTERCARD")
+            print("4")
             return 0
         else:
             print("INVALID")
+            print("5")
             return 0
 
 
