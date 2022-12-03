2887ccc998cca298f0cd4847fc6d13c1a1a3bb33
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-03 15:13:58 +0000

    Sat, Dec 3, 2022, 3:13 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,7 +7,7 @@ def main():
     num_list = re.findall('[0-9]', str(number))
     digits = len(num_list)
     if (digits != 13 and digits != 15 and digits != 16):
-        print(INVALID)
+        print("INVALID")
         return 0
     else:
         first = int(num_list[0])
