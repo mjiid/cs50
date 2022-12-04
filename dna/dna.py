794c2c51b34283801224698e2eabde5587c684dc
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-04 19:26:39 +0000

    Sun, Dec 4, 2022, 7:26 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -25,7 +25,8 @@ def main():
             print(row)
 
     # TODO: Read DNA sequence file into a variable
-
+    with open(sys.argv[2], "r") as file:
+        
     # TODO: Find longest match of each STR in DNA sequence
 
     # TODO: Check database for matching profiles
