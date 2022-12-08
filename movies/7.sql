6b0507cfff7a847cb65c1a1a2ebd01dbfd7b7fb3
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-08 13:30:08 +0000

    Thu, Dec 8, 2022, 1:30 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE year = 2010 ORDER BY rating, title DESC;
+SELECT title, rating FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE year = 2010 ORDER BY rating DESC, title;
