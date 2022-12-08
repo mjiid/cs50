c7ae7bdc1f1994c47aa398d4f36c3ff777a733dd
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-08 20:46:31 +0000

    Thu, Dec 8, 2022, 8:46 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,14 +10,5 @@ SELECT description
 
 
 -- Get the name of the thief who is the one that left the bakery at the time of the steal.
-SELECT name
-  FROM people
-  WHERE license_plate = (
-    SELECT license_plate
-      FROM bakery_security_logs
-      WHERE hour = 10
-      AND minute = 15
-);
-
 
 SELECT name FROM people WHERE passport_number = (SELECT passport_number FROM passengers WHERE flight_id = (SELECT id FROM flights WHERE year = 2021 AND month = 7 AND day = 29 ORDER BY hour, minute LIMIT(1)));
