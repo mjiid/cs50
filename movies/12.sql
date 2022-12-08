fd5e9af672d35700a0349c405413d09597d2b591
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-08 14:24:05 +0000

    Thu, Dec 8, 2022, 2:24 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp" and name = "Helena Bonham Carter"));
+SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp" or name )
