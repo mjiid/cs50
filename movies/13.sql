9b10a0ca6fb9ba69bc93e0270e19e316a9dec6d9
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-08 14:13:58 +0000

    Thu, Dec 8, 2022, 2:13 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE id IN (SELECT p)
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon")));
