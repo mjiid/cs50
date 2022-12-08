2e1a5bf4ca6a95acb36819d4df51175fa283aac9
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-08 13:58:11 +0000

    Thu, Dec 8, 2022, 1:58 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE 
+ SELECT title FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE id IN (SELECT movie_id from stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY(rating) DESC LIMIT(5);
