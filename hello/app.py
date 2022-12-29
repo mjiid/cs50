44c9aa8dbae071e12c3314fd47e95139ff31bbcf
mjiid <100082612+mjiid@users.noreply.github.com>
2022-12-29 19:52:33 +0000

    Thu, Dec 29, 2022, 7:52 PM +00:00

--- hello/app.py
+++ hello/app.py
@@ -4,5 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    name = request.args.get("name", "world")
-    return render_template("index.html", name=name)
+    return render_template("index.html")
