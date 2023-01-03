af50a68d101ae53eb431a40f955ad737ee847403
mjiid <100082612+mjiid@users.noreply.github.com>
2023-01-03 23:02:04 +0000

    Tue, Jan 3, 2023, 11:02 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -195,5 +195,13 @@ def sell():
         print(symbols)
         return render_template("sell.html", symbols = symbols)
     elif request.method == "POST":
-        print("hi")
+        symbol = request.form.get("symbol")
+        print(symbol)
+        shares = request.form.get("shares")
+        print(shares)
+        if symbol not in symbols:
+            return apology("Verify your symbol")
+        elif shares > db.execute("SELECT shares FROM purchases WHERE symbol = ? AND id = ?", symbol, session["user_id]):
+            return apology("You don't own enough shares")
+        return render_template("/")
     return apology("TODO")
