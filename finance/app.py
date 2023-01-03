cbc771d653947d99003f2efcd4aa161d737a8134
mjiid <100082612+mjiid@users.noreply.github.com>
2023-01-03 09:34:41 +0000

    Tue, Jan 3, 2023, 9:34 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -86,8 +86,8 @@ def buy():
             if sym['symbol'] == symbol:
                 owned = True
         if owned:
-            print(db.execute("SELECT shares FROM purchases WHERE symbol = ?", symbol))
-            db.execute("UPDATE purchases shares = ? WHERE symbol = ?", db.execute("SELECT shares FROM purchases WHERE symbol = ?", symbol)[0]['shares'] + shares, symbol)
+            old = db.execute("SELECT shares FROM purchases WHERE symbol = ?", symbol)[0]['shares']
+            db.execute("UPDATE purchases shares = ? WHERE symbol = ?", old + shares, symbol)
         else:
             db.execute("INSERT INTO purchases (id, symbol, price, shares) VALUES (?, ?, ?, ?)", session["user_id"], symbol, price, shares)
         db.execute("UPDATE users SET cash = ? WHERE id = ?", (cash[0]['cash'] - shares * price), session["user_id"])
