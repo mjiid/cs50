09b4e47ca1a2da18592c02725625b191b163aead
mjiid <100082612+mjiid@users.noreply.github.com>
2023-01-04 23:05:17 +0000

    Wed, Jan 4, 2023, 11:05 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -220,7 +220,6 @@ def sell():
             return apology("You don't own enough shares")
         cash = db.execute("SELECT cash FROM users where id = ?", session["user_id"] )
         price = lookup(symbol)['price']
-        db.execute("INSERT INTO purchases (symbol, price, shares, date, operation) VALUES (?, ?, ?, ?, ?) WHERE symbol = ?",symbol, price, shares, date.today(), , symbol)
+        db.execute("INSERT INTO purchases (symbol, price, date, shares, operation) VALUES (?, ?, ?, ?, ?) WHERE symbol = ?",symbol, price, shares, date.today(), (owned_shares[0]['shares'] - shares), "Sold", symbol)
         db.execute("UPDATE users SET cash = ? WHERE id = ?", cash[0]['cash'] + shares * price, session['user_id'])
-        db.execute("INSERT INTO purchases (date, operation) VALUES (?, ?)", date.today(), "Sold")
     return redirect("/")
