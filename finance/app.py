b8cb21ed23e93b96e308b90f71d74776c35497c3
mjiid <100082612+mjiid@users.noreply.github.com>
2023-01-08 09:41:27 +0000

    Sun, Jan 8, 2023, 9:41 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -271,7 +271,7 @@ def sell():
     #The user input is now valid, let's modify the database:
     price = lookup(selected_symbol)['price']
     db.execute("INSERT INTO sells VALUES (?, ?, ?, ?, ?)", username, selected_symbol, price, date.today(), selected_shares)
-    db.execute("UPDATE owned SET shares = shares - ?", selected_shares)
+    db.execute("UPDATE owned SET shares = shares - ?, holding = holding + ?, balance = bla", selected_shares)
 
     return redirect("/")
 
