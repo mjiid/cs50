0178a643b305aacc824f735c27078967ee197bc6
mjiid <100082612+mjiid@users.noreply.github.com>
2023-01-08 22:20:22 +0000

    Sun, Jan 8, 2023, 10:20 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -123,7 +123,7 @@ def history():
     """Show history of transactions"""
     if request.method == "GET":
         username = db.execute("SELECT username FROM users WHERE id = ?", session['user_id'])[0]['username']
-        userActivities = db.execute("SELECT * FROM purchases join sells USING(username) WHERE username = ? ORDER BY date", username)
+        userActivities = db.execute("SELECT * from purchases UNION SELECT * from sells WHERE username = ? ORDER BY date", username)
         print(userActivities)
         return render_template("history.html", data = userActivities)
 
